79a8771a799ffe35f1ab1e454f5e240f6e9783b8
Brayan <brayan.erazo@quind.io>
2024-02-21 11:47:30 -0500

    Se inicia proyecto base y se agrega dependencias necesarias para el funcionamiento de la api

---
+++ src/main/java/io/quind/prueba/prestamolibro/PrestamoLibroApplication.java
@@ -0,0 +1,13 @@
+package io.quind.prueba.prestamolibro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrestamoLibroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrestamoLibroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/quind/prueba/prestamolibro/PrestamoLibroApplicationTests.java
@@ -0,0 +1,13 @@
+package io.quind.prueba.prestamolibro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrestamoLibroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
