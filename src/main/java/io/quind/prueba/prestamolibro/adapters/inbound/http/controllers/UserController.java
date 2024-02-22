e0a6636fabddacff51706a4a90717fd4ba58dc9b
Brayan <brayan.erazo@quind.io>
2024-02-21 23:25:20 -0500

    Se agrega metodo post para crear usuario y constantes

--- src/main/java/io/quind/prueba/prestamolibro/adapters/inbound/http/controllers/UserController.java
+++ src/main/java/io/quind/prueba/prestamolibro/adapters/inbound/http/controllers/UserController.java
@@ -1,9 +1,20 @@
 package io.quind.prueba.prestamolibro.adapters.inbound.http.controllers;
 
+import io.quind.prueba.prestamolibro.adapters.inbound.http.dtos.request.UserRequestDTO;
+import io.quind.prueba.prestamolibro.adapters.inbound.http.dtos.response.UserResponseDTO;
 import io.quind.prueba.prestamolibro.adapters.inbound.http.handlers.IUserHandler;
+import io.quind.prueba.prestamolibro.configuration.Constants;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
 
 @RestController
 @RequestMapping("api/v1/user")
@@ -11,5 +22,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     private final IUserHandler userHandler;
 
-
+    @PostMapping
+    public ResponseEntity<Map<String,String>> createCustomer(@Valid @RequestBody UserRequestDTO userRequestDTO){
+        userHandler.createUser(userRequestDTO);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(Collections.singletonMap(Constants.RESPONSE_MESSAGE_KEY,Constants.USER_CREATED_MESSAGE));
+    }
 }
---
+++ src/main/java/io/quind/prueba/prestamolibro/configuration/Constants.java
@@ -0,0 +1,18 @@
+package io.quind.prueba.prestamolibro.configuration;
+
+public class Constants {
+    private Constants() {
+        throw new IllegalStateException("Utility class");
+    }
+    public static final String RESPONSE_MESSAGE_KEY = "message";
+    public static final String USER_CREATED_MESSAGE = "User created successfully";
+    public static final String USER_UPDATED_MESSAGE = "User updated successfully";
+    public static final String USER_DELETED_MESSAGE = "User deleted successfully";
+
+
+
+
+    public static final String RESPONSE_ERROR_MESSAGE_KEY = "error";
+
+
+}
